557ffef8f1af3c648fb838be78d49d607279cf3b
Victor ADASCALITEI <victor@rrsolutions.it>
2015-06-29 01:41:56 +0200

    Stylized the action bar a little bit to use the appcompat's material design with fdr's blueish colors present in the iOS version of the app. Also added a search menu item with the default icon and expanding to a searchview inside the action bar.

--- app/src/main/java/com/freedomainradio/android/MainActivity.java
+++ app/src/main/java/com/freedomainradio/android/MainActivity.java
@@ -1,17 +1,21 @@
-git statsupackage com.freedomainradio.android;
+package com.freedomainradio.android;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
